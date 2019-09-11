b4cab4569489ac3d3ef10256af071c343b5b6840
Rohan <maharjr@warhawks.ulm.edu>
2019-09-11 00:25:43 -0500

    added authentication

---
+++ csciApp/src/main/java/com/csci4060/app/CsciAppApplication.java
@@ -0,0 +1,13 @@
+package com.csci4060.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsciAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsciAppApplication.class, args);
+	}
+
+}
