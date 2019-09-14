96e1db0b5ee687bd2f1b664ec352943410c68e51
Rohan <maharjr@warhawks.ulm.edu>
2019-09-13 21:15:06 -0500

    added server-side working version of authentication system for localhost

---
+++ working authentication server (localhost)/csciApp/src/main/java/com/csci4060/app/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.csci4060.app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.csci4060.app.model.Role;
+import com.csci4060.app.model.RoleName;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+	Optional<Role> findByName(RoleName roleName); 
+}
