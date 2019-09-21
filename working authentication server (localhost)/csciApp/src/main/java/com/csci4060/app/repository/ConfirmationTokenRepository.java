09703aa5bff9e03365b67b613367b250cd932a8f
Rohan <maharjr@warhawks.ulm.edu>
2019-09-20 21:43:56 -0500

    added service layer

--- working authentication server (localhost)/csciApp/src/main/java/com/csci4060/app/repository/ConfirmationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csci4060.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.csci4060.app.model.ConfirmationToken;
-
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, String>{
-	ConfirmationToken findByConfirmationToken(String confirmationToken);
-}
