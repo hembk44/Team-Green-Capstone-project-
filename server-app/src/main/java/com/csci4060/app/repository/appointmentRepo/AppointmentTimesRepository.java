5679ed6ecff017885b8d77e182bdd074977d56d3
Hem BK <baralhems12@gmail.com>
2019-10-16 20:09:55 -0500

    deleted backend code

--- server-app/src/main/java/com/csci4060/app/repository/appointmentRepo/AppointmentTimesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csci4060.app.repository.appointmentRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.csci4060.app.model.appointment.AppointmentTime;
-
-public interface AppointmentTimesRepository extends JpaRepository<AppointmentTime, Long>{
-
-}
--- server-app/src/main/java/com/csci4060/app/services/EmailSenderService.java
+++
@@ -1,8 +0,0 @@
-package com.csci4060.app.services;
-
-import org.springframework.mail.SimpleMailMessage;
-
-public interface EmailSenderService {
-
-    void sendEmail(SimpleMailMessage email);
-}
