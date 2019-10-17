edd25ba8b203af02454067dd6eba9e4f46e9c0b5
Rohan <maharjr@warhawks.ulm.edu>
2019-10-16 23:00:04 -0500

    deleted

--- server-app/src/main/java/com/csci4060/app/services/AppointmentDateService.java
+++
@@ -1,8 +0,0 @@
-package com.csci4060.app.services;
-
-import com.csci4060.app.model.appointment.AppointmentDate;
-
-public interface AppointmentDateService {
-
-	AppointmentDate save(AppointmentDate appointmentDate);
-}
--- server-app/src/main/java/com/csci4060/app/services/AppointmentTimeService.java
+++
@@ -1,6 +0,0 @@
-package com.csci4060.app.services;
-import com.csci4060.app.model.appointment.AppointmentTime;
-
-public interface AppointmentTimeService {
-	AppointmentTime save(AppointmentTime appointmentTime);
-}
