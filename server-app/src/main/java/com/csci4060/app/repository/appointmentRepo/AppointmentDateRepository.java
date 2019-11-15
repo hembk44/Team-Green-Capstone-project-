866e063fbe029070408cfc1308a3e6c87d6314cf
karkinitesh <niteshkarki19@gmail.com>
2019-11-14 23:35:01 -0600

    deletd file before setting upstream

--- server-app/src/main/java/com/csci4060/app/repository/appointmentRepo/AppointmentDateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csci4060.app.repository.appointmentRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.csci4060.app.model.appointment.AppointmentDate;
-
-public interface AppointmentDateRepository extends JpaRepository<AppointmentDate, Long>{
-
-}
