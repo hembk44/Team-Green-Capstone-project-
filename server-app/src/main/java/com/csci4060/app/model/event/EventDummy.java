82a49f3b9ae427a8b3ae1d61a42f44853678cb8e
Rohan <maharjr@warhawks.ulm.edu>
2019-11-18 23:09:34 -0600

    event validation error fixed

--- server-app/src/main/java/com/csci4060/app/model/event/EventDummy.java
+++ server-app/src/main/java/com/csci4060/app/model/event/EventDummy.java
@@ -3,13 +3,15 @@ package com.csci4060.app.model.event;
 import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
 public class EventDummy {
-	@NotEmpty(message= " Host Calendar must be specified for this event")
+	@NotNull(message= " Host Calendar must be specified for this event")
 	Long calendarId;
 	
 	@NotEmpty(message= "Event title must not be empty")
@@ -19,7 +21,9 @@ public class EventDummy {
 	
 	@NotEmpty(message= "Location must be specified for the event")
 	String location;
+	
 	List<String> recipients;
+	
 	String start;
 	String end;
 	Boolean allDay;
