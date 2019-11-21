8d768c580adc2382f01eb745b9b37780d03f0741
Rohan <maharjr@warhawks.ulm.edu>
2019-11-20 20:09:33 -0600

    calendar can be edited

--- server-app/src/main/java/com/csci4060/app/controller/CalendarController.java
+++ server-app/src/main/java/com/csci4060/app/controller/CalendarController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -95,6 +96,21 @@ public class CalendarController extends ExceptionResolver {
 		Calendar calendar = new Calendar(calendarName, calendarColor, null, recipients, createdBy, true, false);
 
 		calendarService.save(calendar);
+
+		if (!realRecipients.isEmpty()) {
+
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+			String[] emails = realRecipients.toArray(new String[realRecipients.size()]);
+
+			mailMessage.setTo(emails);
+			mailMessage.setSubject("Calendar Added");
+			mailMessage.setFrom("ulmautoemail@gmail.com");
+			mailMessage.setText("This email is to inform you that " + calendar.getCreatedBy().getName()
+					+ " has shared a calendar named " + calendar.getName() + " with you." + "Thank you!");
+
+			emailSenderService.sendEmail(mailMessage);
+		}
 
 		return new APIresponse(HttpStatus.CREATED.value(),
 				"Calendar with name " + calendarName + " has been succesfully created", calendar);
@@ -192,6 +208,123 @@ public class CalendarController extends ExceptionResolver {
 
 		return new APIresponse(HttpStatus.OK.value(),
 				"Calendar " + calendar.getName() + " has been shared to users: " + sharedWithList, calendar);
+	}
+
+	@PutMapping(path = "/edit/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('PM') or hasRole('ADMIN')")
+	public APIresponse editEvent(@Valid @RequestBody CalendarCreate calendarCreate,
+			@PathVariable("id") Long calendarId) {
+
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		String username = "";
+
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails) principal).getUsername();
+		}
+
+		User user = userService.findByUsername(username);
+
+		Calendar calendar = calendarService.findById(calendarId);
+
+		if (calendar == null) {
+			return new APIresponse(HttpStatus.BAD_REQUEST.value(), "Calendar with id " + calendarId + " does not exist",
+					null);
+		}
+
+		if (calendar.getCreatedBy() != user) {
+			return new APIresponse(HttpStatus.FORBIDDEN.value(),
+					"You did not create the Calendar. Authorization denied!", null);
+		}
+
+		String calendarName = calendar.getName();
+		
+		if (!calendarName.equals(calendarCreate.getName())) {
+			if (calendarService.findByNameAndCreatedBy(calendarCreate.getName(), calendar.getCreatedBy()) != null) {
+
+				return new APIresponse(HttpStatus.CONFLICT.value(),
+						"Calendar with name " + calendarCreate.getName() + " already exists.Please choose a different name.",
+						null);
+			}
+		}
+
+		calendar.setName(calendarCreate.getName());
+		
+		String calendarColor = calendarCreate.getColor();
+		
+		if(calendarColor == null) {
+			calendarColor = "#800029";
+		}
+		calendar.setColor(calendarColor);
+
+		List<String> editedEmails = calendarCreate.getRecipients();
+
+		List<User> newRecipients = new ArrayList<>();
+		List<User> oldRecipients = calendar.getShareduser();
+
+		List<User> deletedUsers = new ArrayList<User>();
+		List<String> deletedUserEmail = new ArrayList<String>();
+
+		List<User> addedUsers = new ArrayList<User>();
+		List<String> addedUserEmail = new ArrayList<String>();
+
+		for (String email : editedEmails) {
+			User newRecipientUser = userService.findByEmail(email);
+			if (newRecipientUser != null) {
+				newRecipients.add(newRecipientUser);
+				if (!oldRecipients.contains(newRecipientUser)) {
+					addedUsers.add(newRecipientUser);
+				}
+			}
+		}
+
+		for (User oldRecipient : oldRecipients) {
+			if (!newRecipients.contains(oldRecipient)) {
+				deletedUsers.add(oldRecipient);
+			}
+		}
+
+		for (User newUser : addedUsers) {
+			addedUserEmail.add(newUser.getEmail());
+			calendar.getShareduser().add(newUser);
+		}
+
+		for (User oldUser : deletedUsers) {
+			deletedUserEmail.add(oldUser.getEmail());
+			calendar.getShareduser().remove(oldUser);
+		}
+
+		calendarService.save(calendar);
+
+		if (!addedUserEmail.isEmpty()) {
+
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+			String[] emails = addedUserEmail.toArray(new String[addedUserEmail.size()]);
+
+			mailMessage.setTo(emails);
+			mailMessage.setSubject("Calendar Shared");
+			mailMessage.setFrom("ulmautoemail@gmail.com");
+			mailMessage.setText("This email is to inform you that " + calendar.getCreatedBy().getName()
+					+ " has shared a calendar named " + calendar.getName() + " with you." + "Thank you!");
+			emailSenderService.sendEmail(mailMessage);
+		}
+
+		if (!deletedUserEmail.isEmpty()) {
+
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+			String[] emails = deletedUserEmail.toArray(new String[deletedUserEmail.size()]);
+
+			mailMessage.setTo(emails);
+			mailMessage.setSubject("Calendar removed");
+			mailMessage.setFrom("ulmautoemail@gmail.com");
+			mailMessage.setText("You have been removed from a calendar named " + calendar.getName() + "Thank you!");
+
+			emailSenderService.sendEmail(mailMessage);
+		}
+
+		return new APIresponse(HttpStatus.OK.value(), "Calendar has been successfully edited", calendar);
 	}
 
 	@DeleteMapping(path = "/delete/{id}")
